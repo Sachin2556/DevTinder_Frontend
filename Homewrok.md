ee4dba4204b76b8d9782267e1949ad30a3c38ca0
sachin2556 <rajsachin805130@gmail.com>
2025-02-19 01:47:35 +0530

    fetch_login_api

---
+++ Homewrok.md
@@ -0,0 +1,17 @@
+
+
+- Create a Vite + React application
+- Remove unecessary code and create a Hello World app
+- Install Tailwind CSS  (Install latest version)
+- Install Daisy UI      (Install latest version)
+- Add NavBar component to App.jsx
+- Create a NavBar.jsx separate Component file
+- Install react router dom
+- Create BrowserRouter > Routes > Route=/ Body > RouteChildren
+- Create an Outlet in your Body Component
+- Create a footer
+- Create a Login Page
+- Install axios
+- CORS - install cors in backend => add middleware to with configurations: orgin, credentials: true
+- Whenever you're making API call so pass axios => { withCredentials: true }
+- install react-redux + @reduxjs/toolkit - https://redux-toolkit.js.org/tutorials/quick-start
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,62 @@
-const Login = () =>{
+import { useState } from "react";
+import axios from "axios";
+const Login = () => {
+
+    const [emailId, setEmailId ] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+          
+     try{
+        const res = await axios.post("http://localhost:7777/login", {
+            emailId,
+            password,
+        },
+         { withCredentials: true}  // token will available on browser(cookies will show also which the help of display profile and all of this)
+        );
+      } catch(err) {
+        console.log(err);
+      }
+    };
 
     return (
-       <div>Login Page</div>
+
+        <div className="flex justify-center my-10">
+            <div className="card card-border bg-base-300 w-96 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title justify-center">Login</h2>
+                    <div>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Email ID</legend>
+                            <input 
+                               type="text" 
+                               value={emailId}
+                               className="input" 
+                               onChange={(e) =>  setEmailId(e.target.value)}
+                               
+                               />
+                               
+                           
+                        </fieldset>
+
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Password</legend>
+                            <input 
+                                type="text" 
+                                value = {password}
+                                className="input" 
+                                onChange={(e) => setPassword(e.target.value)}
+                                // binding your input component with changing the names
+                                />
+                           
+                        </fieldset>
+                    </div>
+                    <div className="card-actions justify-center">
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
