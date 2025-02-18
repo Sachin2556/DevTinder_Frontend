3c0e5813e3ca3d215b025fcc79fee72448664645
sachin2556 <rajsachin805130@gmail.com>
2025-02-18 23:35:54 +0530

    Add basic parts

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes,Route } from "react-router";
+
+
+import Body from "./Body";
+import Profile  from "./Profile"
+import Login from "./Login"
+
+
+function App() {
+  
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+         
+            <Route path="/" element={<Body />} >
+               <Route path="/login" element={< Login />}/>
+               <Route path="/profile" element={< Profile />}/>
+            </Route>
+        </Routes>
+      
+      </BrowserRouter>
+
+      {/* <NavBar /> */}
+        
+    </>
+  );
+}
+
+export default App
