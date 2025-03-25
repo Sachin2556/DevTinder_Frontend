2dfd8a1b35e9f15a4a2e00e054e774dc0f0aa4ff
sachin2556 <rajsachin805130@gmail.com>
2025-03-25 23:37:09 +0530

    feat: Connections_Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./components/Login"
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Feed from "./components/Feed.jsx";
+import Connections from "./components/Connections.jsx";
 
 function App() {
   
@@ -20,6 +21,7 @@ function App() {
                <Route path="/" element={<Feed />} />
                <Route path="/login" element={< Login />}/>
                <Route path="/profile" element={< Profile />}/>
+               <Route path="/connections" element={< Connections />}/>
             </Route>
         </Routes>
       
